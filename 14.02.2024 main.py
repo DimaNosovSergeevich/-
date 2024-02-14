4b0d4af6357d9ff1f5c62be9dec44237e66807e6
DimaNosovSergeevich <158263672+DimaNosovSergeevich@users.noreply.github.com>
2024-02-14 11:49:44 +0300

    14.02.2024 Практическая работа

---
+++ 14.02.2024 main.py
@@ -0,0 +1,24 @@
+_coding: utf-8
+
+Задача 1
+r=int(input())
+if r > 0:
+    print("Число больше нуля")
+elif r < 0:
+    print("Число меньше нуля")
+    
+Задача 2    
+red=int(input())
+if red > 0:
+    print("1")
+else:
+    print("-1")
+    
+Задача 3:
+ku=input() ("Выйдешь гулять? (Да, Нет, ХЗ")
+if ku == "Да":
+    print("Го")
+elif ku == "ХЗ":
+    print('Да пошел ты')
+else:
+    print("Блин, ну ладно")
