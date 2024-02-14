e942e110052f05aeb2319ee9a1d39a2bfab9457f
DimaNosovSergeevich <158263672+DimaNosovSergeevich@users.noreply.github.com>
2024-02-14 11:55:25 +0300

    14.02.2024 main.py

--- 14.02.2024 main.py
+++ 14.02.2024 main.py
@@ -1,20 +1,20 @@
 #coding: utf-8
 
-Задача 1
+#Задача 1
 r=int(input())
 if r > 0:
     print("Число больше нуля")
 elif r < 0:
     print("Число меньше нуля")
     
-Задача 2    
+#Задача 2    
 red=int(input())
 if red > 0:
     print("1")
 else:
     print("-1")
     
-Задача 3:
+#Задача 3:
 ku=input() ("Выйдешь гулять? (Да, Нет, ХЗ")
 if ku == "Да":
     print("Го")
@@ -22,3 +22,42 @@ elif ku == "ХЗ":
     print('Да пошел ты')
 else:
     print("Блин, ну ладно")
+
+#Задача 4
+a=int(input())
+b=int(input())
+if a > b:
+    c = a-b
+elif a < b:
+    c = a+b
+else:
+    c = a
+print(c)
+
+#Задача 5
+v=int(input()) ("введите любые числа от 12 до 23")
+if v==12:
+    print("12 апреля - день всех линивый")
+elif v==13:
+    print("Пятница 13")
+else:
+    print("Ты полюбому ввел 14")
+if v==15:
+    print:("15")
+elif v==16:
+    print("Мне в пршлом году было 16")
+else:
+    print("Я знаю что ты ввел 17 я отвечаю")
+if v==18:
+    print("18+ контент")
+elif v == 19:
+    print("А тебе точно 19")
+else:
+    print("Да Да это явно 20")
+if v==21:
+    print("20 + 1")
+elif v==22:
+    print("22 номер")
+else:
+    print("так и знал что напишешь 23")
+
